8b7c6554afe159b9cb7465f6d6e74c3d8f871e50
Param Aggarwal <paramaggarwal@gmail.com>
2014-04-03 19:09:42 +0530

    Initial Commit

---
+++ ToDoList/TDLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TDLAppDelegate.h
+//  ToDoList
+//
+//  Created by Param Aggarwal on 03/04/14.
+//  Copyright (c) 2014 Param Aggarwal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TDLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ToDoList/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ToDoList
+//
+//  Created by Param Aggarwal on 03/04/14.
+//  Copyright (c) 2014 Param Aggarwal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TDLAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TDLAppDelegate class]));
+    }
+}
