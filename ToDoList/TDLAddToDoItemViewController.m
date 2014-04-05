bb8b02600111430c37cb78e2ee27e93583e88170
Param Aggarwal <paramaggarwal@gmail.com>
2014-04-05 19:19:52 +0530

    add new item

--- ToDoList/TDLAddToDoItemViewController.m
+++ ToDoList/TDLAddToDoItemViewController.m
@@ -10,6 +10,9 @@
 
 @interface TDLAddToDoItemViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *textField;
+@property (weak, nonatomic) IBOutlet UIBarButtonItem *doneButton;
+
 @end
 
 @implementation TDLAddToDoItemViewController
@@ -35,15 +38,18 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
-{
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
+{    
+    if (sender != self.doneButton) return;
+    
+    if (self.textField.text.length > 0) {
+        self.toDoItem = [[TDLToDoItem alloc] init];
+        self.toDoItem.itemName = self.textField.text;
+        self.toDoItem.completed = NO;
+    }
 }
-*/
 
 @end
--- ToDoList/TDLToDoListTableViewController.m
+++ ToDoList/TDLToDoListTableViewController.m
@@ -8,6 +8,7 @@
 
 #import "TDLToDoListTableViewController.h"
 #import "TDLToDoItem.h"
+#import "TDLAddToDoItemViewController.h"
 
 @interface TDLToDoListTableViewController ()
 
@@ -34,7 +35,13 @@
 
 - (IBAction)unwindToList:(UIStoryboardSegue *)segue
 {
-
+    TDLAddToDoItemViewController *source = [segue sourceViewController];
+    TDLToDoItem *item = source.toDoItem;
+    
+    if (item != nil) {
+        [self.toDoItems addObject:item];
+        [self.tableView reloadData];
+    }
 }
 
 - (id)initWithStyle:(UITableViewStyle)style
